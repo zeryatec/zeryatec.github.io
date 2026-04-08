c727128073b38e203a19a8f812af796beb4d60e0
AlvaroPM <alvaropavonmartinez7@gmail.com>
2026-04-08 18:08:56 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/zeryatec',
   images: {
     unoptimized: true,
   },
